eb8c0de07fd3a338020f8cc981bee7ba063c0655
Jin young <xmfkdnak98@gmail.com>
2024-02-29 00:40:44 +0900

    style: 마우스 오버 스타일 추가

--- js/homePage.js
+++ js/homePage.js
@@ -1,10 +1,11 @@
 
 const homePage = () => {
   const wrap = document.createElement('div');
-  const test = document.createElement('h1');
-  
-  test.textContent = "테스트페이지 입니다.";
-  wrap.appendChild(test);
+  for(let i=0;i<50;i++){
+    const test = document.createElement('h1');
+    test.textContent = "테스트페이지 입니다.";
+    wrap.appendChild(test);
+  }
 
   return wrap;
 } 
--- js/mouse.js
+++ js/mouse.js
@@ -1,10 +1,13 @@
 const cursor = document.getElementsByClassName('cursor')[0];
+
 document.addEventListener("mousemove", (e)=>{
   const mouseX = e.clientX;
   const mouseY = e.clientY;
+  
   cursor.style.left = `${mouseX}px`;
-    cursor.style.top = `${mouseY}px`;
-  })
+  cursor.style.top = `${mouseY}px`;
+})
+
 document.addEventListener("mouseleave", ()=>{
   cursor.classList.add('hidden')
 })
@@ -12,3 +15,16 @@ document.addEventListener("mouseenter", ()=>{
   cursor.classList.remove('hidden')
 })
 
+const hover = document.getElementsByTagName('a');
+
+for(let i=0;i<hover.length;i++){
+  hover[i].addEventListener('mouseover', ()=>{
+    cursor.classList.add('hover');
+  })
+}
+
+for(let i=0;i<hover.length;i++){
+  hover[i].addEventListener('mouseout', ()=>{
+    cursor.classList.remove('hover');
+  })
+}
