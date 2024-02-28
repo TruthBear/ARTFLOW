fdb9e2bc9ed0dade7515053e9311873da0099818
Jin young <xmfkdnak98@gmail.com>
2024-02-29 08:01:04 +0900

    add 동영상 추가

--- js/homePage.js
+++ js/homePage.js
@@ -3,13 +3,15 @@ const homePage = () => {
   const wrap = document.createElement('div');
   wrap.classList.add('bg-video')
 
+
+  // 가로 무한 슬라이드
   const slider = document.createElement('div');
   slider.classList.add('slider');
 
   const slideTrack = document.createElement('div');
   slideTrack.classList.add('slide-track');
 
-  const slideArr = ["Live art performance unfolds", "Live art performance unfolds"];
+  const slideArr = ["A R T F L O W", "A R T F L O W"];
   slideArr.forEach((item)=> {
     const slide = document.createElement('div');
     slide.textContent = item;
@@ -20,6 +22,36 @@ const homePage = () => {
   slider.appendChild(slideTrack);
 
   wrap.appendChild(slider);
+
+
+  // 동그라미 버튼
+  const circleBox = document.createElement('div');
+  circleBox.classList.add('circle-box');
+
+  const circleBoxWrap = document.createElement('div');
+  circleBoxWrap.classList.add('circle-box-wrap');
+
+  const circleTextArr = ["공연", "시설", "About"];
+
+  circleTextArr.forEach(item => {
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+
+    const circleWrap = document.createElement('div');
+    circleWrap.classList.add('circle-wrap');
+
+    const circleText = document.createElement('a');
+    circleText.classList.add('circle-text');
+    circleText.textContent = item;
+
+    circleWrap.appendChild(circleText);
+    circle.appendChild(circleWrap);
+    circleBoxWrap.appendChild(circle);
+  })
+
+  circleBox.appendChild(circleBoxWrap);
+  wrap.appendChild(circleBox);
+
 
   return wrap;
 } 
